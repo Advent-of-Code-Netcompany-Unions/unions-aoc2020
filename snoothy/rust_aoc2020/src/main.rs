dc874e70162cf34934af638b6ffb80fc777a138e
Snoothy <kai.ejler.rasmussen@gmail.com>
2020-12-03 23:07:27 +0100

    One day of rust

--- snoothy/rust_aoc2020/src/main.rs
+++ snoothy/rust_aoc2020/src/main.rs
@@ -5,16 +5,34 @@ use std::path::Path;
 fn main() {
     println!("Rust AoC");
 
+    aoc01();
+
+}
+
+fn aoc01() -> () {
     let filepath = "./input/input_01a.txt";
 
     let numbers = lines_from_file(&filepath);
+
+    let mut result1 : i32 = 0;
+    let mut result2 : i32 = 0;
+
     for x in &numbers {
         for y in &numbers {
             if x+y == 2020 {
-                println!("AoC 1.1 Result: {:?}", x * y);
+                result1 = x * y;
+
+            }
+            for z in &numbers {
+                if x+y+z == 2020 {
+                    result2 = x * y * z;
+                }
             }
         }
     }
+
+    println!("AoC 1.1 Result: {:?}", result1);
+    println!("AoC 1.2 Result: {:?}", result2);
 }
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
