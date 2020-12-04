9b3a279b350910696c131ccbfd474907944eb409
Anders Thavlov <anth@netcompany.com>
2020-12-04 14:59:28 +0100

    ANTH: Solution day 4

--- anth/adventofcode2020/src/main/java/com/thavlov/aoc/AdventOfCode2020.java
+++ anth/adventofcode2020/src/main/java/com/thavlov/aoc/AdventOfCode2020.java
@@ -6,6 +6,8 @@ import com.thavlov.aoc.day2.Day2Ex1;
 import com.thavlov.aoc.day2.Day2Ex2;
 import com.thavlov.aoc.day3.Day3Ex1;
 import com.thavlov.aoc.day3.Day3Ex2;
+import com.thavlov.aoc.day4.Day4Ex1;
+import com.thavlov.aoc.day4.Day4Ex2;
 
 public class AdventOfCode2020 {
     public static void main(String[] args) throws Exception {
@@ -22,6 +24,9 @@ public class AdventOfCode2020 {
         System.out.println(" > Solution 1 to day 3: " + Day3Ex1.solve());
         System.out.println(" > Solution 2 to day 3: " + Day3Ex2.solve());
 
-        System.out.println(String.format("Execution time: %d ms.",(System.currentTimeMillis()-t)));
+        System.out.println(" > Solution 1 to day 4: " + Day4Ex1.solve());
+        System.out.println(" > Solution 2 to day 4: " + Day4Ex2.solve());
+
+        System.out.println(String.format("Execution time: %d ms.", (System.currentTimeMillis() - t)));
     }
 }
