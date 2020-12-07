56beb47cb6d98a1cd8b715bfbc96c0eb97d1ce0a
Rasmus Wismann <rlw@netcompany.com>
2020-12-07 21:08:11 +0100

    Recursion FTW

--- rlw/src/com/wismann/aoc/day7/Bag.java
+++ rlw/src/com/wismann/aoc/day7/Bag.java
@@ -3,7 +3,7 @@ package com.wismann.aoc.day7;
 import java.util.Map;
 
 public class Bag {
-    private String bagColor;
+    private final String bagColor;
 
     private Map<Bag, Integer> containedBags;
 
@@ -15,15 +15,23 @@ public class Bag {
         return bagColor;
     }
 
-    public Map<Bag, Integer> getContainedBags() {
-        return containedBags;
-    }
-
-    public void setBagColor(String bagColor) {
-        this.bagColor = bagColor;
-    }
-
     public void setContainedBags(Map<Bag, Integer> containedBags) {
         this.containedBags = containedBags;
+    }
+
+    public boolean containsShinyBag() {
+        return containedBags.keySet().stream()
+                .anyMatch(cb -> cb.containsShinyBag() || cb.getBagColor().equals("shiny gold"));
+    }
+
+    public int countSubBags() {
+        int directSubBagCount = containedBags.values().stream()
+                .reduce(0, (i1, i2) -> i1 + i2);
+
+        int nextLevelSubBagCount = containedBags.entrySet().stream()
+                .map(e -> e.getKey().countSubBags() * e.getValue())
+                .reduce(0, (i1, i2) -> i1 + i2);
+
+        return directSubBagCount + nextLevelSubBagCount;
     }
 }
--- rlw/src/com/wismann/aoc/day7/Main.java
+++ rlw/src/com/wismann/aoc/day7/Main.java
@@ -11,25 +11,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String filename = "resources/day7.txt";
 
-        createBags(filename);
-    }
-
-    private static void createBags(String inputFile) throws IOException {
-        List<String> lines = Files.lines(Paths.get(inputFile))
+        List<String> lines = Files.lines(Paths.get(filename))
                 .collect(Collectors.toList());
 
-        List<Bag> bags = lines.stream()
+        List<Bag> bags = buildBags(lines);
+
+        System.out.println(solveFirstProblem(bags));
+        System.out.println(solveSecondProblem(bags));
+
+    }
+    private static long solveSecondProblem(List<Bag> bags) {
+        return bags.stream().filter(b -> b.getBagColor().equals("shiny gold")).findFirst().map(Bag::countSubBags).orElse(0);
+    }
+
+    private static long solveFirstProblem(List<Bag> bags) {
+        return bags.stream().filter(Bag::containsShinyBag).count();
+    }
+
+    private static List<Bag> buildBags(List<String> inputLines) {
+        List<Bag> bags = inputLines.stream()
                 .map(l -> l.split("bags")[0].trim())
                 .map(Bag::new)
                 .collect(Collectors.toList());
 
-       lines.forEach(l -> setContainedBags(bags, l));
+        inputLines.forEach(l -> buildBag(bags, l));
+
+        return bags;
     }
 
-    private static void setContainedBags(List<Bag> bags, String inputLine) {
 
+    private static void buildBag(List<Bag> bags, String inputLine) {
         String mainBagColor = inputLine.split("bags")[0].trim();
-        Bag mainBag = bags.stream().filter(b -> b.getBagColor().equals(mainBagColor)).findFirst().orElseThrow();
+        Bag mainBag = bags.stream().filter(b -> b.getBagColor().equals(mainBagColor)).findFirst().orElse(null);
 
         String[] contains = inputLine.split("contain")[1].trim().split(",");
         List<String> containedBagStrings = Arrays.stream(contains)
@@ -37,6 +50,21 @@ public class Main {
                 .map(s -> s.replaceAll("bags", ""))
                 .map(String::trim)
                 .collect(Collectors.toList());
+
+        Map<Bag, Integer> containedBags = new HashMap<>();
+
+        for (String s : containedBagStrings) {
+            if (s.equals("no other")) {
+                break;
+            }
+            String[] split = s.split(" ");
+            int amount = Integer.parseInt(split[0]);
+            String color = split[1] + " " + split[2];
+            Bag bag = bags.stream().filter(b -> b.getBagColor().equals(color)).findFirst().orElse(null);
+            containedBags.put(bag, amount);
+        }
+
+        mainBag.setContainedBags(containedBags);
     }
 
 }
