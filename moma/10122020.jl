2442eba51c07ac7bd61d7d7917e64aaacd7187f5
Morten Madsen <morten.madsen.92@gmail.com>
2020-12-10 21:22:15 +0100

    cleaned up pt1 and pt2 a tiny bit

--- moma/10122020.jl
+++ moma/10122020.jl
@@ -1,26 +1,26 @@
-function connect_adapters(joltage_adapters::Array{Int64})::Dict{String, Int64}
-    jolt_differences = Dict{String, Int64}("1-jolt"=>0, "2-jolt"=>0, "3-jolt"=>0)
+function is_joltage_within_range(current_joltage::Int64, new_joltage::Int64)::Bool
+    return 3 >= (new_joltage - current_joltage) >= 1
+end
 
-    previous_rating = 0
-    for joltage_adapter ∈ joltage_adapters
-        current_rating = joltage_adapter
+function connect_adapters(joltage_adapters::Array{Int64})::Dict{Int64, Int64}
+    jolt_differences = Dict{Int64, Int64}()
 
-        rating_difference = current_rating - previous_rating
-
-        if (rating_difference > 3 || rating_difference < 1)
-            return Dict{String, Int64}()
+    previous_joltage = 0
+    for current_joltage ∈ joltage_adapters
+        joltage_difference = current_joltage - previous_joltage
+        if (haskey(jolt_differences, joltage_difference))
+            jolt_differences[joltage_difference] += 1
+        else
+            jolt_differences[joltage_difference] = 1
         end
-
-        key = "$rating_difference-jolt"
-        jolt_differences[key] += 1
-        previous_rating = current_rating
+        previous_joltage = current_joltage
     end   
 
     return jolt_differences
 end
 
 function sum_paths(joltage_adapters::Array{Int64}, current_joltage::Int64, visited::Dict{Int64, Int64} = Dict{Int64, Int64}())::Int64
-    neighbours = findall(x -> 3 >= (x - current_joltage) >= 1, joltage_adapters)
+    neighbours = findall(x -> is_joltage_within_range(current_joltage, x), joltage_adapters)
 
     if (isempty(neighbours))
         return 1
@@ -36,11 +36,7 @@ function sum_paths(joltage_adapters::Array{Int64}, current_joltage::Int64, visit
         end
     end
 
-    if (haskey(visited, current_joltage))
-        visited[current_joltage] += paths
-    else
-        visited[current_joltage] = paths
-    end
+    visited[current_joltage] = paths
 
     return paths
 end
@@ -49,7 +45,7 @@ function charge_my_device(joltage_adapters::Array{Int64}, puzzle_part::Int64)::I
     push!(joltage_adapters, maximum(joltage_adapters)+3)
     if (puzzle_part == 1)
         jolt_differences = connect_adapters(joltage_adapters)
-        return jolt_differences["1-jolt"] * jolt_differences["3-jolt"]
+        return jolt_differences[1] * jolt_differences[3]
     else
         return sum_paths(joltage_adapters, 0)
     end
@@ -60,4 +56,4 @@ end
 data = "resources/10122020/input.txt"
 puzzle_part = 2
 joltage_adapters = sort(parse.(Int64, readlines(data)))
-@show charge_my_device(joltage_adapters, puzzle_part)
+@btime charge_my_device(joltage_adapters, puzzle_part)
