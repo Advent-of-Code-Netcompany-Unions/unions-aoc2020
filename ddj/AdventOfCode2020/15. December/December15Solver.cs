7bec87773f01dce10b258d4996e583f7abea774e
Dada <juhl.daniel@gmail.com>
2020-12-15 22:26:09 +0100

    Nice and easy today, so lets ruin it by unnecessary optimizations. I blame chce.

---
+++ ddj/AdventOfCode2020/15. December/December15Solver.cs
@@ -0,0 +1,74 @@
+﻿using AoCLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _15._December
+{
+    class December15Solver : ChallengeSolver
+    {
+        private Dictionary<long, (int, int)> Memory;
+        private long LastNumberSpoken;
+
+        public override async Task Solve(string filename)
+        {           
+            var input = await InputHelper.ReadStrings(filename);
+            var startingNumbers = input.First().Split(",").Select(long.Parse).ToList();
+
+            Console.WriteLine($"Res1: {Run(startingNumbers, 2020)}");
+            Console.WriteLine($"Res2: {Run(startingNumbers, 30000000)}");           
+        }
+
+        public long Run(List<long> startingNumbers, int targetTurn)
+        {
+            Memory = new Dictionary<long, (int, int)>();
+
+            var turn = 1;
+            while (turn <= startingNumbers.Count)
+            {
+                LastNumberSpoken = startingNumbers[turn - 1];
+                Memory[LastNumberSpoken] = (turn, -1);
+                turn++;
+            }
+
+            while (turn <= targetTurn)
+            {
+                TakeTurn(turn);
+                turn++;
+            }
+
+            return LastNumberSpoken;
+        }
+
+        public void Say(long num, int turn)
+        {
+            LastNumberSpoken = num;
+
+            if (Memory.ContainsKey(num))
+            {
+                var temp = Memory[num];
+                Memory[num] = (turn, temp.Item1);
+            }
+            else
+            {
+                Memory[num] = (turn, -1);
+            }
+            
+        }
+
+        public void TakeTurn(int turn)
+        {
+            if (Memory.ContainsKey(LastNumberSpoken))
+            {
+                var numberInfo = Memory[LastNumberSpoken];
+                if (numberInfo.Item2 > 0)
+                {
+                    Say(numberInfo.Item1 - numberInfo.Item2, turn);
+                    return;
+                }
+            }
+            Say(0, turn);
+        }        
+    }
+}
---
+++ ddj/AdventOfCode2020/15. December/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace _15._December
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var solver = new December15Solver();
+            await solver.Run();
+
+            Console.WriteLine($"Ok, now run optimized version...");
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var res1 = OptimizedSolve(2020);
+            var res2 = OptimizedSolve(30000000);            
+            stopWatch.Stop();
+            Console.WriteLine($"Found result: {res1} to part 1 and {res2} to part 2 in: {stopWatch.ElapsedMilliseconds} ms.");
+            Console.WriteLine();
+        }
+
+        static long OptimizedSolve(int targetTurn)
+        {
+            var shortMemory = new int[targetTurn];            
+            var longMemory = new int[targetTurn];            
+
+            /*Abusing input being small enough to manually set up*/
+            //1,0,18,10,19,6
+            shortMemory[1] = 1;
+            shortMemory[0] = 2;
+            shortMemory[18] = 3;
+            shortMemory[10] = 4;
+            shortMemory[19] = 5;
+            shortMemory[6] = 6;
+            var lastNumberSpoken = 6;
+
+            var turn = 7;
+            while (turn <= targetTurn)
+            {
+                var longMemoryVal = longMemory[lastNumberSpoken];
+                if (longMemoryVal > 0)
+                {
+                    lastNumberSpoken = shortMemory[lastNumberSpoken] - longMemoryVal;
+                    longMemory[lastNumberSpoken] = shortMemory[lastNumberSpoken];
+                    shortMemory[lastNumberSpoken] = turn;
+                    turn++;
+                    continue;
+                }
+                lastNumberSpoken = 0;
+                longMemory[0] = shortMemory[0];
+                shortMemory[0] = turn;
+                turn++;
+            }
+
+            return lastNumberSpoken;
+        }      
+    }
+}
